c8a05c853d48e2fe306df402d7cb15082aadc53a
Sullivan Young <syoung@costar.com>
2023-03-09 17:28:14 -0500

    start chatbot ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [history, setHistory] = useState([]);
+
+  const getAnswer = async () => {
+    // const data = {
+    //   question,
+    // };
+    // await axios
+    //   .post('http://localhost:5000/get-bot-response', data)
+    //   .then((response) => {
+    //     console.log(response);
+    //     setAnswer(response.data.answer);
+    //   })
+    //   .catch((error) => console.log(error));
+
+    setAnswer('Thank you for testing me');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar"></div>
+              <div className="message">User Chat</div>
+            </div>
+          </div>
+          <div className="chat-message chatbot">
+            <div className="chat-message-center">
+              <div className="avatar">add costar svg here</div>
+              <div className="message">Bot Chat</div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea
+            rows={1}
+            className="chat-input-text"
+            placeholder="Please Ask Your Benefits Question"
+          ></textarea>
+        </div>
+      </section>
     </div>
+    // <form
+    //   onSubmit={(e) => {
+    //     e.preventDefault();
+    //   }}
+    // >
+    //   <div>
+    //     <label>Question: </label>
+    //     <input
+    //       type="text"
+    //       value={question}
+    //       onChange={(event) => setQuestion(event.target.value)}
+    //     />
+    //     <button onClick={getAnswer}>Get Answer</button>
+    //     <br />
+    //     <label>Answer: </label>
+    //     <p>{answer}</p>
+    //   </div>
+    // </form>
   );
 }
 
